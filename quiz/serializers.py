09df1104f856da64ed02918f80edc06f749111ca
Er. Bom Bahadur B.K <52617074+bombk@users.noreply.github.com>
2026-08-24 13:20:04 +0545

    Optimize quiz serializers: remove expensive question counts

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -1,30 +1,24 @@
 from rest_framework import serializers
 from .models import Category, SubCategory, PaperCategory, Question, Option, QuestionPaper, UserProgress
 
-class PaperCategorySerializer(serializers.ModelSerializer):
-    paper_count = serializers.SerializerMethodField()
 
+class PaperCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = PaperCategory
-        fields = ['id', 'name', 'slug', 'description', 'image', 'paper_count']
+        fields = ['id', 'name', 'slug', 'description', 'image']
 
-    def get_paper_count(self, obj):
-        return obj.papers.count()
 
 class SubCategorySerializer(serializers.ModelSerializer):
-    question_count = serializers.SerializerMethodField()
-
     class Meta:
         model = SubCategory
-        fields = ['id', 'name', 'slug', 'description', 'question_count']
+        fields = ['id', 'name', 'slug', 'description']
 
-    def get_question_count(self, obj):
-        return obj.questions.count()
 
 class OptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Option
         fields = ['id', 'text', 'is_correct']
+
 
 class QuestionSerializer(serializers.ModelSerializer):
     options = OptionSerializer(many=True, read_only=True)
@@ -32,28 +26,34 @@ class QuestionSerializer(serializers.ModelSerializer):
     category_slug = serializers.ReadOnlyField(source='category.slug')
     sub_category_name = serializers.ReadOnlyField(source='sub_category.name')
     sub_category_slug = serializers.ReadOnlyField(source='sub_category.slug')
-    
+
     class Meta:
         model = Question
-        fields = ['id', 'category', 'category_name', 'category_slug', 'sub_category', 'sub_category_name', 'sub_category_slug', 'text', 'explanation', 'options']
+        fields = [
+            'id', 'category', 'category_name', 'category_slug',
+            'sub_category', 'sub_category_name', 'sub_category_slug',
+            'text', 'explanation', 'options'
+        ]
+
 
 class CategorySerializer(serializers.ModelSerializer):
-    question_count = serializers.SerializerMethodField()
     subcategories = SubCategorySerializer(many=True, read_only=True)
 
     class Meta:
         model = Category
-        fields = ['id', 'name', 'slug', 'description', 'image', 'question_count', 'subcategories']
+        fields = ['id', 'name', 'slug', 'description', 'image', 'subcategories']
 
-    def get_question_count(self, obj):
-        return obj.questions.count()
 
 class QuestionPaperSerializer(serializers.ModelSerializer):
     category_name = serializers.ReadOnlyField(source='category.name')
-    
+
     class Meta:
         model = QuestionPaper
-        fields = ['id', 'title', 'category', 'category_name', 'file', 'image', 'description', 'uploaded_at']
+        fields = [
+            'id', 'title', 'category', 'category_name', 'file',
+            'image', 'description', 'uploaded_at'
+        ]
+
 
 class UserProgressSerializer(serializers.ModelSerializer):
     class Meta:
